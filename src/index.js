96f96748719b8efd7a9918ce5955fda825d5ddd4
Natalie <natali14441@gmail.com>
2020-03-21 17:31:23 +0300

    feat: add reverse number function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    
+    if (n < 10) return n;
+
+    let first = Math.floor(n / 10);
+    let last = n % 10; 
+    let length = Math.pow(10, n.toString().length - 1);
+
+    return last * length + reverse(first);
 }
